e18f5d9a7151bf574eae8db811a1d0bd8aa44d68
patrick simmons <hadroncollides@gmail.com>
2021-01-26 18:37:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,15 @@
 # don't forget to add: require 'pry'
+require 'pry'
+player1 = "Rocky"
+player2 = "Bullwinkle"
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+  "Hello" #{#layer1} & #{player2}
+  puts "hi"
 end
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
+  binding.pry
+  puts "hi"
 end
